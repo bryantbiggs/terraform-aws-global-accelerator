8c060e227e8ed1caddc353f7648d21ed40fbc37b
Bryant Biggs <bryantbiggs@gmail.com>
2022-11-15 16:37:17 -0500

    feat!: Allow setting multiple endpoint groups on a listener

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -2,9 +2,25 @@ provider "aws" {
   region = local.region
 }
 
+provider "aws" {
+  region = "eu-west-1"
+  alias  = "euwest1"
+}
+
+data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "euwest1_available" {
+  provider = aws.euwest1
+}
+
 locals {
   region = "us-east-1"
   name   = "global-acclerator-ex-${replace(basename(path.cwd), "_", "-")}"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  euwest1_vpc_cidr = "10.20.0.0/16"
+  euwest1_azs      = slice(data.aws_availability_zones.euwest1_available.names, 0, 3)
 
   tags = {
     Name       = local.name
@@ -16,81 +32,8 @@ locals {
 data "aws_caller_identity" "current" {}
 
 ################################################################################
-# Supporting Resources
-################################################################################
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 3.0"
-
-  name = local.name
-  cidr = "10.99.0.0/18"
-
-  azs            = ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnets = ["10.99.0.0/24", "10.99.1.0/24", "10.99.2.0/24"]
-
-  tags = local.tags
-}
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 7.0"
-
-  name               = local.name
-  load_balancer_type = "application"
-
-  vpc_id          = module.vpc.vpc_id
-  subnets         = module.vpc.public_subnets
-  security_groups = [module.vpc.default_security_group_id]
-
-  http_tcp_listeners = [{
-    port               = 80
-    protocol           = "HTTP"
-    target_group_index = 0
-  }]
-
-  target_groups = [{
-    backend_protocol = "HTTP"
-    backend_port     = 80
-    target_type      = "ip"
-  }]
-
-  tags = local.tags
-}
-
-module "s3_log_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 3.0"
-
-  bucket = "${local.name}-flowlogs-${data.aws_caller_identity.current.account_id}-${local.region}"
-  acl    = "log-delivery-write"
-
-  # Not recommended for production, required for testing
-  force_destroy = true
-
-  # Provides the necessary policy for flow logs to be delivered
-  attach_lb_log_delivery_policy = true
-
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  tags = local.tags
-}
-
-################################################################################
 # Global Accelerator Module
 ################################################################################
-
-module "global_accelerator_disabled" {
-  source = "../.."
-
-  create = false
-}
 
 module "global_accelerator" {
   source = "../.."
@@ -106,40 +49,6 @@ module "global_accelerator" {
     listener_1 = {
       client_affinity = "SOURCE_IP"
 
-      endpoint_group = {
-        health_check_port             = 80
-        health_check_protocol         = "HTTP"
-        health_check_path             = "/"
-        health_check_interval_seconds = 10
-        health_check_timeout_seconds  = 5
-        healthy_threshold_count       = 2
-        unhealthy_threshold_count     = 2
-        traffic_dial_percentage       = 100
-
-        # Health checks will show as unhealthy in this example because there
-        # are obviously no healthy instances in the target group
-        endpoint_configuration = [{
-          client_ip_preservation_enabled = true
-          endpoint_id                    = module.alb.lb_arn
-          weight                         = 50
-          }, {
-          client_ip_preservation_enabled = false
-          endpoint_id                    = module.alb.lb_arn
-          weight                         = 50
-        }]
-
-        port_override = [{
-          endpoint_port = 82
-          listener_port = 80
-          }, {
-          endpoint_port = 8082
-          listener_port = 8080
-          }, {
-          endpoint_port = 8083
-          listener_port = 8081
-        }]
-      }
-
       port_ranges = [
         {
           from_port = 80
@@ -151,6 +60,77 @@ module "global_accelerator" {
         }
       ]
       protocol = "TCP"
+
+      endpoint_groups = {
+        useast1 = {
+          endpoint_group_region         = "us-east-1"
+          health_check_port             = 80
+          health_check_protocol         = "HTTP"
+          health_check_path             = "/"
+          health_check_interval_seconds = 10
+          health_check_timeout_seconds  = 5
+          healthy_threshold_count       = 2
+          unhealthy_threshold_count     = 2
+          traffic_dial_percentage       = 50
+
+          # Health checks will show as unhealthy in this example because there
+          # are obviously no healthy instances in the target group
+          endpoint_configuration = [{
+            client_ip_preservation_enabled = true
+            endpoint_id                    = module.alb.lb_arn
+            weight                         = 50
+            }, {
+            client_ip_preservation_enabled = false
+            endpoint_id                    = module.alb.lb_arn
+            weight                         = 50
+          }]
+
+          port_override = [{
+            endpoint_port = 82
+            listener_port = 80
+            }, {
+            endpoint_port = 8082
+            listener_port = 8080
+            }, {
+            endpoint_port = 8083
+            listener_port = 8081
+          }]
+        }
+        euwest1 = {
+          endpoint_group_region         = "eu-west-1"
+          health_check_port             = 81
+          health_check_protocol         = "HTTP"
+          health_check_path             = "/"
+          health_check_interval_seconds = 10
+          health_check_timeout_seconds  = 5
+          healthy_threshold_count       = 2
+          unhealthy_threshold_count     = 2
+          traffic_dial_percentage       = 50
+
+          # Health checks will show as unhealthy in this example because there
+          # are obviously no healthy instances in the target group
+          endpoint_configuration = [{
+            client_ip_preservation_enabled = true
+            endpoint_id                    = module.euwest1_alb.lb_arn
+            weight                         = 50
+            }, {
+            client_ip_preservation_enabled = false
+            endpoint_id                    = module.euwest1_alb.lb_arn
+            weight                         = 50
+          }]
+
+          port_override = [{
+            endpoint_port = 82
+            listener_port = 80
+            }, {
+            endpoint_port = 8082
+            listener_port = 8080
+            }, {
+            endpoint_port = 8083
+            listener_port = 8081
+          }]
+        }
+      }
     }
 
     listener_2 = {
@@ -189,6 +169,126 @@ module "global_accelerator" {
     update = "35m"
     delete = "35m"
   }
+
+  tags = local.tags
+}
+
+module "global_accelerator_disabled" {
+  source = "../.."
+
+  create = false
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs            = local.azs
+  public_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+
+  tags = local.tags
+}
+
+module "euwest1_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
+
+  providers = {
+    aws = aws.euwest1
+  }
+
+  name = local.name
+  cidr = local.euwest1_vpc_cidr
+
+  azs            = local.euwest1_azs
+  public_subnets = [for k, v in local.euwest1_azs : cidrsubnet(local.euwest1_vpc_cidr, 8, k)]
+
+  tags = local.tags
+}
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 7.0"
+
+  name               = local.name
+  load_balancer_type = "application"
+
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.vpc.default_security_group_id]
+
+  http_tcp_listeners = [{
+    port               = 80
+    protocol           = "HTTP"
+    target_group_index = 0
+  }]
+
+  target_groups = [{
+    backend_protocol = "HTTP"
+    backend_port     = 80
+    target_type      = "ip"
+  }]
+
+  tags = local.tags
+}
+
+module "euwest1_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 7.0"
+
+  providers = {
+    aws = aws.euwest1
+  }
+
+  name               = local.name
+  load_balancer_type = "application"
+
+  vpc_id          = module.euwest1_vpc.vpc_id
+  subnets         = module.euwest1_vpc.public_subnets
+  security_groups = [module.euwest1_vpc.default_security_group_id]
+
+  http_tcp_listeners = [{
+    port               = 80
+    protocol           = "HTTP"
+    target_group_index = 0
+  }]
+
+  target_groups = [{
+    backend_protocol = "HTTP"
+    backend_port     = 80
+    target_type      = "ip"
+  }]
+
+  tags = local.tags
+}
+
+module "s3_log_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket = "${local.name}-flowlogs-${data.aws_caller_identity.current.account_id}-${local.region}"
+  acl    = "log-delivery-write"
+
+  # Not recommended for production, required for testing
+  force_destroy = true
+
+  # Provides the necessary policy for flow logs to be delivered
+  attach_lb_log_delivery_policy = true
+
+  attach_deny_insecure_transport_policy = true
+  attach_require_latest_tls_policy      = true
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 
   tags = local.tags
 }
--- main.tf
+++ main.tf
@@ -25,25 +25,29 @@ resource "aws_globalaccelerator_accelerator" "this" {
 # Listener(s)
 ################################################################################
 
+locals {
+  create_listeners = var.create && var.create_listeners
+}
+
 resource "aws_globalaccelerator_listener" "this" {
-  for_each = { for k, v in var.listeners : k => v if var.create && var.create_listeners }
+  for_each = { for k, v in var.listeners : k => v if local.create_listeners }
 
   accelerator_arn = aws_globalaccelerator_accelerator.this[0].id
-  client_affinity = lookup(each.value, "client_affinity", null)
-  protocol        = lookup(each.value, "protocol", null)
+  client_affinity = try(each.value.client_affinity, null)
+  protocol        = try(each.value.protocol, null)
 
   dynamic "port_range" {
-    for_each = try(each.value.port_ranges, null) != null ? each.value.port_ranges : []
+    for_each = try(each.value.port_ranges, [])
     content {
-      from_port = lookup(port_range.value, "from_port", null)
-      to_port   = lookup(port_range.value, "to_port", null)
+      from_port = try(port_range.value.from_port, null)
+      to_port   = try(port_range.value.to_port, null)
     }
   }
 
   timeouts {
-    create = lookup(var.listeners_timeouts, "create", null)
-    update = lookup(var.listeners_timeouts, "update", null)
-    delete = lookup(var.listeners_timeouts, "delete", null)
+    create = try(var.listeners_timeouts.create, null)
+    update = try(var.listeners_timeouts.update, null)
+    delete = try(var.listeners_timeouts.delete, null)
   }
 }
 
@@ -52,20 +56,20 @@ resource "aws_globalaccelerator_listener" "this" {
 ################################################################################
 
 resource "aws_globalaccelerator_endpoint_group" "this" {
-  for_each = { for k, v in var.listeners : k => v if var.create && var.create_listeners && length(lookup(var.listeners[k], "endpoint_group", {})) >= 0 }
+  for_each = { for k, v in var.listeners : k => { for k2, v2 in try(v.endpoint_groups, {}) : k2 => v2 } if local.create_listeners }
 
   listener_arn = aws_globalaccelerator_listener.this[each.key].id
 
-  endpoint_group_region         = try(each.value.endpoint_group.endpoint_group_region, null)
-  health_check_interval_seconds = try(each.value.endpoint_group.health_check_interval_seconds, null)
-  health_check_path             = try(each.value.endpoint_group.health_check_path, null)
-  health_check_port             = try(each.value.endpoint_group.health_check_port, null)
-  health_check_protocol         = try(each.value.endpoint_group.health_check_protocol, null)
-  threshold_count               = try(each.value.endpoint_group.threshold_count, null)
-  traffic_dial_percentage       = try(each.value.endpoint_group.traffic_dial_percentage, null)
+  endpoint_group_region         = try(each.value.endpoint_group_region, null)
+  health_check_interval_seconds = try(each.value.health_check_interval_seconds, null)
+  health_check_path             = try(each.value.health_check_path, null)
+  health_check_port             = try(each.value.health_check_port, null)
+  health_check_protocol         = try(each.value.health_check_protocol, null)
+  threshold_count               = try(each.value.threshold_count, null)
+  traffic_dial_percentage       = try(each.value.traffic_dial_percentage, null)
 
   dynamic "endpoint_configuration" {
-    for_each = [for e in try(each.value.endpoint_group.endpoint_configuration, []) : e if can(e.endpoint_id)]
+    for_each = try(each.value.endpoint_configuration, [])
     content {
       client_ip_preservation_enabled = try(endpoint_configuration.value.client_ip_preservation_enabled, null)
       endpoint_id                    = endpoint_configuration.value.endpoint_id
@@ -74,7 +78,7 @@ resource "aws_globalaccelerator_endpoint_group" "this" {
   }
 
   dynamic "port_override" {
-    for_each = can(each.value.endpoint_group.port_override) ? each.value.endpoint_group.port_override : []
+    for_each = try(each.value.endpoint_group.port_override, [])
     content {
       endpoint_port = port_override.value.endpoint_port
       listener_port = port_override.value.listener_port
@@ -82,8 +86,8 @@ resource "aws_globalaccelerator_endpoint_group" "this" {
   }
 
   timeouts {
-    create = lookup(var.endpoint_groups_timeouts, "create", null)
-    update = lookup(var.endpoint_groups_timeouts, "update", null)
-    delete = lookup(var.endpoint_groups_timeouts, "delete", null)
+    create = try(var.endpoint_groups_timeouts.create, null)
+    update = try(var.endpoint_groups_timeouts.update, null)
+    delete = try(var.endpoint_groups_timeouts.delete, null)
   }
 }
